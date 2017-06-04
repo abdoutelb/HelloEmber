e8a417b6292d83b85ff554f73066a5f93e31a65a
Abd Elrahman Telb <abdoutelb.fci@gmail.com>
2017-06-04 17:19:09 +0200

    Test the parent route and child route

--- app/router.js
+++ app/router.js
@@ -8,6 +8,8 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('parent');
+
+  this.route('parent.child',{path : "parent/child"});
 });
 
 export default Router;
